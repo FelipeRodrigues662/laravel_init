5ce64bdff9443f0dbf67d2c7a8895687e1567f88
FelipeRodrigues662 <felipe.rod00@hotmail.com>
2024-03-27 23:19:30 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,55 @@ Route::get('/empresa', function(){
     return view('site/empresa');
 });
 
+//Todos os verbos Http
 Route::any('/any', function(){
     return "Todas os verbos Http";
 });
 
+//Escolhe mais de um método Http para essa rota
 Route::match(['get', 'post'],'/match', function(){
     return "Apenas métodos permitidos";
 });
 
+//Passagem de parametros
 Route::get('/produto/{id}', function($id){
     return "O id do produto é: " .$id;
+});
+
+Route::get('/notPermission/{id}', function($id){
+    return "O Id: ".$id." não é permitido.";
+});
+
+//Redirect
+Route::prefix('sobre')->group(function(){
+    Route::get('/idEmpresa/{id}', function($id){
+
+        if($id > 100){
+            return redirect('/produto'.'/'.$id);
+        }
+        else
+            return redirect('/notPermission'.'/'.$id);
+    });
+});
+
+
+// Route::redirect('/empresa', '/site/empresa');
+// Route::view('/empresa', '/site/empresa');
+
+Route::get('/news', function(){
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function () {
+    return redirect()->route('noticias');
+});
+
+
+Route::group([
+    'prefix'=> 'admin',
+    'as'=>'admin.'
+], function(){
+    Route::get('user', function(){
+        return "User";
+    })->name('userTeste');
 });
